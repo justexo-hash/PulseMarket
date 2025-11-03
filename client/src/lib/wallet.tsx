d90e568abf0335924e30a5e70ceaa663e6908c24
Burnit <64872188+justexo-hash@users.noreply.github.com>
2025-11-03 05:24:37 +0100

    Replace wallet-adapter-wallets meta-package with individual adapters to avoid usb dependency

--- client/src/lib/wallet.tsx
+++ client/src/lib/wallet.tsx
@@ -2,11 +2,9 @@ import { useMemo, ReactNode } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-  TorusWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
+import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare';
+import { TorusWalletAdapter } from '@solana/wallet-adapter-torus';
 import { clusterApiUrl } from '@solana/web3.js';
 
 // Import wallet adapter CSS
