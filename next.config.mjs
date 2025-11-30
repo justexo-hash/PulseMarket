236eb3982654c68b0b77d887cb009ad986232456
Burnit <64872188+justexo-hash@users.noreply.github.com>
2025-11-30 06:36:48 +0100

    fix: allow remote profile images in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "localhost",
+      "127.0.0.1",
+      "pulsemkt.app",
+      "pulsemkt.vercel.app",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
